253ef1a8e0e66001f00c051722d91c041ea4423c
Keith Thompson <Keith.S.Thompson@gmail.com>
2025-12-31 15:24:24 -0800

    Show message for non-zero errno

--- caller.c
+++ caller.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 #include <unistd.h>
 int main(void) {
     extern char **environ;
     char *arg0 = NULL;
     const int result = execve("./callee", &arg0, environ);
-    printf("caller: execve returned %d, errno = %d\n", result, errno);
+    printf("caller: execve returned %d, errno = %d, %s\n",
+           result, errno, strerror(errno));
 }
