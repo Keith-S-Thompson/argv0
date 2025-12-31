98395b168388c1c9b25343e26daa94372fd0893b
Keith Thompson <Keith.S.Thompson@gmail.com>
2025-12-31 15:21:48 -0800

    Show result of failing execve() call

--- caller.c
+++ caller.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
 int main(void) {
     extern char **environ;
     char *arg0 = NULL;
-    execve("./callee", &arg0, environ);
+    const int result = execve("./callee", &arg0, environ);
+    printf("caller: execve returned %d, errno = %d\n", result, errno);
 }
